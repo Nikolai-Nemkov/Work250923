8a53cc8bdbb073cc4459553495dd3e62393affe0
Николай <nnm_58@mail.ru>
2023-09-25 20:34:17 +0300

    Добавил Задачу №2

--- work2/Program.cs
+++ work2/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача №1");
+
+Console.WriteLine("Введите Ваше имя!");
+string username = Console.ReadLine();
+Console.WriteLine("Приветик, ");
+Console.WriteLine(username);
