043a5da169df85ba9cf6c4639f3a1b7a53754961
Николай <nnm_58@mail.ru>
2023-09-25 21:30:30 +0300

    Добавил Задачу №6

--- work6/Program.cs
+++ work6/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача №6");
+
+// Приветствуем одного из пользователе  по особенному, как любимчика 
+              // все работает
+
+Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username == "Коля")  // if(username.ToLower() == "Коля") позволяет не ошибиться
+                         // при вводе имени с большой или маленькой буквы.
+{
+ Console.WriteLine("Ура это же Коля!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
