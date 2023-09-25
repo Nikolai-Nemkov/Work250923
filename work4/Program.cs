907dd6a56b28aaa49f15b4ada3ca1d7de510ef80
Николай <nnm_58@mail.ru>
2023-09-25 21:02:28 +0300

    Добавил типы данных, математические операции

--- work4/Program.cs
+++ work4/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача №4");
+
+// int работает с целыми числами, double c  дробными, пример ниже
+
+double numberA = 19;
+double numberB = 3;
+Console.WriteLine(numberA / numberB);
