7b330936dbc365d3abbe6717c4d512f20c2d0062
Николай <nnm_58@mail.ru>
2023-09-25 20:26:08 +0300

    Добавил обертку С#

--- work1/Program.cs
+++ work1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+isung System;     // isung -(использование) System -(название библиотеки, в данном случае System)
+             // ниже, пространство имен, в данном случае указывает что все это относится к папке Task1 и уст {}
+namespace Task1{
+                    // ниже class и название файла в папке Task1
+    class Program{ 
+
+        static vold Main(){
+
+            Console.WriteLine("Ввдите число");
+        }   
+    }
+}
