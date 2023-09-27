d4a1b0d5e40e859577fd2e02acd7a94bb548ad79
Николай <nnm_58@mail.ru>
2023-09-27 12:34:20 +0300

    Создал папку №9

--- work8/Program.cs
+++ work8/Program.cs
@@ -1,2 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача №8");
+
+
+  //                               Не сработал
+// Задача про точки. Берем две точки ставим еще одну по середине, затем следующу и т.д. Цикл
+// console.Clear();  // Очистка консоли
+// console.SetCursorPosition(10, 4); // отступы строками от левого края и отступ от лверхнего края. Позиция курсора
+// Console.WriteLine("+");   // рисовка + (плюсиками)
+// Определяем вершины треугольника
+
+int xa = 40;
+    ya = 1;
+    xb = 1;
+    yb = 1;
+    xc = 80; 
+    yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+"); 
+
+Console.SetCursorPosition(xb, xb);
+Console.WriteLine("+"); 
+ 
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+"); 
+
+int x = xa, y = xb; //  случайные точки
+
+int count = 0;  // ставим счетчик на цикл количества точек
+
+while(count < 10000)  // ставим счетчик на цикл количества точек
+{
+    int what = new Random().Next(0,3); //
+    if(what == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+    if(what == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+    if(what == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+"); 
+    count++;
+
+}
---
+++ work9/Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(); // пробел
+Console.WriteLine("Задача №9");
+Console.WriteLine(); // пробел
+
+// Таблица умножения 1
+
+for (int i = 1; i <= 1; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 2
+
+for (int i = 2; i <= 2; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 3
+
+
+for (int i = 3; i <= 3; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 4
+
+for (int i = 4; i <= 4; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 5
+
+for (int i = 5; i <= 5; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 6
+
+
+
+for (int i = 6; i <= 6; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 7
+
+for (int i = 7; i <= 7; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Таблица умножения 8
+
+for (int i = 8; i <= 9; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
